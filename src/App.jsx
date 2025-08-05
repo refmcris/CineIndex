5f836b7c28e570012210c7989cf544a53279e174
refmcris <cristianmartineztez@gmail.com>
2025-08-04 21:30:47 -0500

    Vite files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>CineSearch</h1>
+      <h3>Soon!</h3>
+      
+    </>
+  )
+}
+
+export default App
